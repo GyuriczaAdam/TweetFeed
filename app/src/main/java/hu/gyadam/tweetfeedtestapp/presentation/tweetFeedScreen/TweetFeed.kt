b03b21db6a4f04da09fa89fd15664f788fb17cdd
suhf96 <suhf.adam@gmail.com>
2023-05-11 11:55:50 +0200

    reformat code

--- app/src/main/java/hu/gyadam/tweetfeedtestapp/presentation/tweetFeedScreen/TweetFeed.kt
+++ app/src/main/java/hu/gyadam/tweetfeedtestapp/presentation/tweetFeedScreen/TweetFeed.kt
@@ -1,6 +1,5 @@
 package hu.gyadam.tweetfeedtestapp.presentation.tweetFeedScreen
 
-import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,22 +8,18 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -35,7 +30,6 @@ import hu.gyadam.tweetfeedtestapp.domain.observer.ConnectivityObserver
 import hu.gyadam.tweetfeedtestapp.presentation.tweetFeedScreen.components.SearchTextField
 import hu.gyadam.tweetfeedtestapp.presentation.tweetFeedScreen.components.TweetCard
 import hu.gyadam.tweetfeedtestapp.presentation.util.LocalSpacing
-import hu.gyadam.tweetfeedtestapp.presentation.util.UiEvent
 
 @Composable
 fun TweetFeed(
